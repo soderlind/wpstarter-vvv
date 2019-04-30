e6c6062f90dacb8aae7304ef0a380b35adb3dc30
soderlind <per@soderlind.no>
2019-04-30 22:05:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# WP Starter 3.0 - VVV site template
+
+under development.
+
+1) [Install VVV](https://varyingvagrantvagrants.org/docs/en-US/installation/)
+
+2) To be able to clone the private repositories:
+	- [Add a new SSH key](https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/) to your GitHub account.
+	- Set up SSH forwarding by [adding your SSH key to the ssh-agent](https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/#adding-your-ssh-key-to-the-ssh-agent)
+
+3) In the local VVV folder, create the following vvv-custom.yml file:
+
+	```yml
+	sites:
+	  wpstarter:
+	    repo: https://github.com/soderlind/wpstarter-vvv
+	    nginx_upstream: php73
+	    hosts:
+	      - wpstarter.local
+	    custom:
+	      wp_type: subdirectory
+	      wp_version: latest
+	      acf_pro_key: acf_pro_license_key
+
+	vm_config:
+	  memory: 2048
+	  cores: 2
+
+	utilities:
+	  core:
+	    - php73
+
+	utility-sources:
+	  core: https://github.com/Varying-Vagrant-Vagrants/vvv-utilities.git
+	```
+
+4) In the local VVV folder run `vagrant up`, or `vagrant reload --provision` if vagrant is already running.
---
+++ provision/vvv-init.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# Provision WordPress Stable
+
+
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".local`
+DOMAINS=`get_hosts "${DOMAIN}"`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+WP_VERSION=`get_config_value 'wp_version' 'latest'`
+WP_TYPE=`get_config_value 'wp_type' "single"`
+DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+ACF_PRO_KEY=`get_config_value 'acf_pro_key' "xyz"`
+
+#
+# nginx
+#
+
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+
+# Clone the WP Starter config repo
+if [[ ! -f "${VVV_PATH_TO_SITE}/wpstarter.json" ]]; then
+    echo "Cloning WP Starter config..."
+	noroot git clone https://github.com/soderlind/wpstarter-config "${VVV_PATH_TO_SITE}/"
+
+	echo "Configure .env"
+
+	sed -i "s#site_name_here#${DOMAIN}#" "${VVV_PATH_TO_SITE}/.env-sample"
+	sed -i "s#db_name_here#${DB_NAME}#" "${VVV_PATH_TO_SITE}/.env-sample"
+	sed -i "s#db_username_here#wp#" "${VVV_PATH_TO_SITE}/.env-sample"
+	sed -i "s#db_password_here#wp#" "${VVV_PATH_TO_SITE}/.env-sample"
+	sed -i "s#acf_pro_key_here#${ACF_PRO_KEY}#" "${VVV_PATH_TO_SITE}/.env-sample"
+
+	noroot mv "${VVV_PATH_TO_SITE}/.env-sample" "${VVV_PATH_TO_SITE}/.env"
+else
+	cd ${VVV_PATH_TO_SITE}
+	noroot git pull
+fi
+
+
+# Make a database, if we don't already have one
+echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\n DB operations done.\n\n"
+
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
+
+
+# Composer
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts ssh -Ts git@github.com
+cd ${VVV_PATH_TO_SITE}/public
+if [[ ! -d "${VVV_PATH_TO_SITE}/vendor" ]]; then
+	echo "Running composer install"
+	noroot composer install
+else
+	echo "Running composer update"
+	noroot composer update
+fi
